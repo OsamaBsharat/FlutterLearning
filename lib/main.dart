d640bdef29d9edf5116a277867a6f9abb32ee1de
Osama Bsharat <bsharat1999@gmail.com>
2024-03-08 00:51:24 +0200

    button-text-commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,96 @@
-import 'package:flutter/material.dart';
+import 'dart:math';
 
-main() {
-  runApp(
-    Container(
-      margin: const EdgeInsets.all(10.0),
-      color: Colors.amber[600],
-      width: 48.0,
-      height: 48.0,
-    ),
-  );
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'OsamaShaheenFirstAPP',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(
+              seedColor: Color.fromARGB(255, 31, 203, 255)),
+        ),
+        home: MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyAppState extends ChangeNotifier {
+  List<String> strings = [
+    'اسامة',
+    'بشارات',
+    'فلسطين',
+    'رقم الهاتف : 00970594145999',
+    'مهندس حاسوب',
+    'مطور تطبيقات هواتف محمولة'
+  ];
+  void getNext() {
+    notifyListeners();
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    Random random = Random();
+    var current = appState.strings[random.nextInt(6) + 0];
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            BigCard(current: current),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                appState.getNext();
+              },
+              child: Text('! انطلق'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.current,
+  });
+
+  final String current;
+
+  @override
+  Widget build(BuildContext context) {
+    var theme = Theme.of(context);
+    var style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onSecondary,
+    );
+    return Card(
+      color: theme.colorScheme.primary,
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Text(
+          current,
+          style: style,
+        ),
+      ),
+    );
+  }
 }
