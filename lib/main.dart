0c5c08f59d53923b30523a401e03cac4b4549801
Osama Bsharat <bsharat1999@gmail.com>
2024-03-07 23:28:46 +0200

    first-commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(
+    Container(
+      margin: const EdgeInsets.all(10.0),
+      color: Colors.amber[600],
+      width: 48.0,
+      height: 48.0,
+    ),
+  );
+}
